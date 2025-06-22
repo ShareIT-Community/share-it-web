15707044659d19e332ca29e8adb544b5a32960e9
Víctor Quiñones <victhorq716@gmail.com>
2025-06-21 20:17:44 -0400

    [refactor] --Updated the community description in the Layout.astro meta and improved the presentation of important information in aboutUs.astro. Removed obsolete sections in aboutUs.ts to simplify the code.

--- src/utils/aboutUs.ts
+++ src/utils/aboutUs.ts
@@ -24,12 +24,6 @@ export const groups = [
     size: 'sm:col-span-1 sm:row-span-1',
   },
   {
-    title: '💻🖊️ Proyectos:',
-    description:
-      'Si necesitas ayuda con un proyecto personal, o estás buscando equipo para construir tu próxima app, este es el lugar indicado.',
-    size: 'sm:col-span-1 sm:row-span-1',
-  },
-  {
     title: '🌎📝 Networking y CV:',
     description:
       'El espacio ideal para que compartas tus redes sociales y conozcas a otros profesionales de la industria. También vas a poder solicitar una revisión de tu CV para poder mejorarlo y aumentar tus probabilidades de encontrar ese ansiado empleo.',
@@ -51,12 +45,6 @@ export const groups = [
     title: '☕💧 La Llorería:',
     description:
       'A veces necesitamos hacer catarsis de ese compañero que no nos escucha en el trabajo, de aquel profesor que pide cosas imposibles o cuando te ghostean en un proceso laboral. ¡Acá tenés el lugar indicado para descargarte!',
-    size: 'sm:col-span-1 sm:row-span-1',
-  },
-  {
-    title: '🇺🇸 English Practice:',
-    description:
-      '¿Te gustaría aprender/practicar/refinar tu inglés? En este espacio vas a poder enviar mensajes de texto y/o voz e interactuar con otras personas que comparten el mismo objetivo, que es dominar el idioma más utilizado a nivel mundial. También desde este lugar organizamos llamadas semanales en nuestro servidor de Discord para tener charlas en tiempo real.',
     size: 'sm:col-span-2 sm:row-span-2',
   },
 ]
@@ -89,15 +77,6 @@ export const admins = [
     website: 'https://linktr.ee/shinigamy19',
   },
   {
-    name: 'Ana Rivarola',
-    about: 'Admin',
-    role: 'Frontend | CM | Tester Qa',
-    avatar: '/images/ana-avatar.jpg',
-    github: 'https://github.com/Anita1555',
-    linkedin: 'https://www.linkedin.com/in/anabelenrivarola/',
-    instagram: 'https://www.instagram.com/anibel1555?igsh=MXB3NWMxb3gycHFyZQ==',
-  },
-  {
     name: 'Jean Roa (Drak)',
     about: 'Admin',
     role: 'Software Developer',
@@ -105,13 +84,6 @@ export const admins = [
     github: 'https://github.com/jeanroadev',
     linkedin: 'https://www.linkedin.com/in/jeanmra/',
     website: 'https://jeanroa.dev/',
-  },
-  {
-    name: 'Claudia Metz',
-    about: 'Admin',
-    role: ' Data Scientist',
-    avatar: '/images/claudia-avatar.jpg',
-    linkedin: 'https://www.linkedin.com/in/claudia-metz/',
   },
   {
     name: 'Nicolás Cruz (Nico)',
@@ -122,20 +94,5 @@ export const admins = [
     linkedin: 'https://www.linkedin.com/in/nicofj-cruz',
     website: 'http://nicolascruz.jovinet.com/',
     twitter: 'https://x.com/NicoCruzDev',
-  },
-  {
-    name: 'Virginia García',
-    about: 'Admin',
-    role: 'Analista Funcional',
-    avatar: '/images/virginia-avatar.jpg',
-    github: 'https://github.com/virginia-garcia',
-    linkedin: 'https://www.linkedin.com/in/garcia-virginia/',
-  },
-  {
-    name: 'Fernando Cardozo',
-    about: 'Admin',
-    role: 'Professional Fisherman',
-    avatar: '/images/fernando-avatar.jpeg',
-    twitter: 'https://x.com/f_l_cl',
   },
 ]
