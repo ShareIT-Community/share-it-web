8565ec45483642d6b6f408d36f8508071b3baa75
AzzADesigns <azariiielll@gmail.com>
2025-07-23 22:07:04 -0300

    [feat] -- added logic to open and close form

---
+++ src/components/EntryForm/EntryForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ButtonCloseForm } from './ButtonCloseForm'
+
+export const EntryForm = () => {
+    
+  return (
+    <div className='h-120 w-120  justify-center items-center'>
+        <ButtonCloseForm/>    
+        EntryForm
+    </div>
+  )
+}
