ec01abad32328fd4fcf18bfdc93be671461dc654
AzzADesigns <azariiielll@gmail.com>
2025-07-28 22:14:47 -0300

    featured: Added the ability to view rules before submitting the form.

--- src/components/RulesTimelines/RulesTimelines.tsx
+++ src/components/RulesTimelines/RulesTimelines.tsx
@@ -19,6 +19,7 @@ export const RulesTimelines = () => {
 			<ul className='flex items-center justify-center mb-8 text-sm font-medium text-center text-gray-400'>
 				<li className='me-2'>
 					<button
+						type="button"
 						className={`inline-flex items-center justify-center p-4 border-b-2  rounded-t-lg hover:text-cyan-300 hover:border-cyan-300 font-semibold cursor-pointer ${
 							tabOption === 'general'
 								? 'text-cyan-300 border-cyan-300'
@@ -31,6 +32,7 @@ export const RulesTimelines = () => {
 
 				<li className='me-2'>
 					<button
+						type="button"
 						className={`inline-flex items-center justify-center p-4 border-b-2  rounded-t-lg hover:text-cyan-300 hover:border-cyan-300 font-semibold cursor-pointer ${
 							tabOption === 'groups'
 								? 'text-cyan-300 border-cyan-300'
