4244c2ad32ed8fc9ddd3645c6d6f19fc85acf4c8
AzzADesigns <azariiielll@gmail.com>
2025-08-16 15:23:45 -0300

    fix(form): fix data submission to backend and community invite validation
    Resolved an issue where the form was not correctly sending data to the backend.
    Now the backend properly receives the submitted information.
    Additionally, the community invitation no longer always appears, but instead follows the proper validations.

--- src/components/Toasts/notifications.tsx
+++ src/components/Toasts/notifications.tsx
@@ -1,20 +1,6 @@
-import { toast } from "react-toastify";
+
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
-
-export const notifications = {
-  errorForm: "⚠️ Error al llenar el formulario. Por favor, modifica los campos solicitados.",
-};
-
-export type NotificationType = keyof typeof notifications;
-
-export function showNotification(type: NotificationType) {
-  const content = notifications[type] || "Tienes un error";
-  toast(<div>{content}</div>, {
-    autoClose: 3000,
-    pauseOnHover: true,
-  });
-}
 
 export const NotificationsContainer = () => (
   <ToastContainer
