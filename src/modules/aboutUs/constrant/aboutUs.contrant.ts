339ef94442cea89bab489c84487b3d78a02c1858
Alejandro <aletomasperren@gmail.com>
2025-10-13 16:52:29 -0300

    Refactor Components to Modules About Us

---
+++ src/modules/aboutUs/constrant/aboutUs.contrant.ts
@@ -0,0 +1,103 @@
+import { FaDiscord } from "react-icons/fa6";
+import { GoRocket } from "react-icons/go";
+export const socialLinks = {
+  discord: {
+    title: 'Discord de la comunidad',
+    description: 'Únete a la comunidad',
+    url: 'https://discord.gg/8cvhbu7sgn',
+    icon: FaDiscord,
+  },
+  github: {
+    title: 'GitHub de la comunidad',
+    description: 'Nuestras redes',
+    url: 'https://bento.me/shareit',
+    icon: GoRocket,
+  },
+};
+
+export const groups = [
+  {
+    title: '👋🗣️ General',
+    description:
+      'El grupo donde nos presentamos y hablamos de cualquier tema que nos interese.',
+    size: 'sm:col-span-1 sm:row-span-1',
+  },
+  {
+    title: '🌎📝 Networking, Eventos y CV',
+    description:
+      'Conecta con otros profesionales de la industria y comparte tus redes sociales. Obtén feedback sobre tu CV para maximizar tus oportunidades laborales. También podrás organizar y difundir eventos IT.',
+    size: 'sm:col-span-1 sm:row-span-1',
+  },
+  {
+    title: '📚 Recursos',
+    description:
+      'En este grupo compartimos aquellos recursos (videos, blog posts, enlaces, etc) que nos permitan mejorar nuestros conocimientos.',
+    size: 'sm:col-span-1 sm:row-span-1',
+  },
+  {
+    title: '🆘 Mesa de Ayuda',
+    description:
+      'El grupo ideal para pedir ayuda y resolver tus dudas cuando Google, ChatGPT o StackOverflow no tienen la respuesta. También es el espacio para compartir tus propios conocimientos y ayudar a otros.',
+    size: 'sm:col-span-1 sm:row-span-1',
+  },
+  {
+    title: '💼🤝🏻 Ofertas Laborales',
+    description:
+      'Tal como el nombre lo indica, este grupo es utilizado para compartir ofertas laborales orientadas al sector tecnológico. ¿Y si tu próximo destino laboral está en ShareIT?',
+    size: 'sm:col-span-1 sm:row-span-1',
+  },
+  {
+    title: '☕💧 La Llorería',
+    description:
+      'A veces necesitamos hacer catarsis de ese compañero que no nos escucha en el trabajo, de aquel profesor que pide cosas imposibles o cuando nos ghostean en un proceso laboral. ¡Acá tenés el lugar indicado para descargarte!',
+    size: 'sm:col-span-1 sm:row-span-1',
+  },
+];
+
+export const admins = [
+  {
+    name: 'Elias Velázquez (Kani)',
+    about: 'Fundador de la Comunidad',
+    role: 'Data Engineer / Developer',
+    avatar: '/images/kani-avatar.png',
+    github: 'https://github.com/eliasvelazquezdev',
+    linkedin: 'https://linkedin.com/in/eliassvelazquez',
+    website: 'http://evelazquez.dev/',
+    twitter: 'https://x.com/esvdev',
+  },
+  {
+    name: 'Natalia Quevedo (Brooke)',
+    about: 'Admin',
+    role: 'UX/UI Designer',
+    avatar: '/images/naty-avatar.jpg',
+    linkedin: 'https://www.linkedin.com/in/natalia-a-quevedo/',
+  },
+  {
+    name: 'Eros Benitez (Shini)',
+    about: 'Admin',
+    role: 'Diseñador Multimedia',
+    avatar: '/images/eros-avatar.jpg',
+    github: 'https://github.com/Shinigamy19',
+    linkedin: 'https://www.linkedin.com/in/eros-benitez/',
+    website: 'https://linktr.ee/shinigamy19',
+  },
+  {
+    name: 'Jean Roa (Drak)',
+    about: 'Admin',
+    role: 'Software Developer',
+    avatar: '/images/drak-avatar.jpeg',
+    github: 'https://github.com/jeanroadev',
+    linkedin: 'https://www.linkedin.com/in/jeanmra/',
+    website: 'https://jeanroa.dev/',
+  },
+  {
+    name:"Azariel Moreno (Azza)",
+    about:"admin",
+    role:'Full-Stack Developer',
+    avatar:'/images/azza-avatar.jpg',
+    github:'https://github.com/AzzADesigns',
+    linkedin:'https://www.linkedin.com/in/azariel-moreno-4267ba254/',
+    website:'https://www.azzadesigns.dev/'
+  }
+]
+
