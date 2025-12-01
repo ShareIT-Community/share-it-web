b704e98703ad2f8a5adcaff0a8807b184eeef956
Ulises <ulisesapaza.280@gmail.com>
2025-12-01 16:03:30 -0300

    fix: use env variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,18 @@ import netlify from '@astrojs/netlify';
 
 import sanity from '@sanity/astro';
 
+import {loadEnv} from "vite";
+
+const {
+  PUBLIC_SANITY_PROJECT_ID,
+  PUBLIC_SANITY_DATASET,
+} = loadEnv(process.env.NODE_ENV, process.cwd, "");
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), sanity({
-    projectId: "bhiv53i7",
-    dataset: "production",
+    projectId: PUBLIC_SANITY_PROJECT_ID,
+    dataset: PUBLIC_SANITY_DATASET,
     useCdn: false,
     apiVersion: "2025-11-30",
   })],
