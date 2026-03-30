324bbef0acbe4529017bc4876193285b4456e553
Victor Quinones <victhorq716@gmail.com>
2026-03-30 17:31:08 -0400

    feat: integrate particles effect in hero section and update styles
    - Added ParticlesHero component to enhance visual appeal on the homepage.
    - Updated JoinButton component to use rounded-md for consistent styling.
    - Refactored SocialLinks component for improved readability and formatting.

--- src/modules/global/components/Header/JoinButton.tsx
+++ src/modules/global/components/Header/JoinButton.tsx
@@ -17,7 +17,7 @@ export const JoinButton: React.FC = () => {
 		<>
 			<button
 				onClick={() => setIsFormOpen(true)}
-				className="flex items-center justify-center gap-2 w-full md:w-auto px-4 py-2 rounded-xl bg-orange-500 text-white font-semibold hover:opacity-90 transition-all cursor-pointer"
+				className="flex items-center justify-center gap-2 w-full md:w-auto px-4 py-2 rounded-md bg-orange-500 text-white font-semibold hover:opacity-90 transition-all cursor-pointer"
 			>
 				<span>Únete</span>
 
