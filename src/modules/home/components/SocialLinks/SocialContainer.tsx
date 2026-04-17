1f14e4bc0f26db691fc22e827fc4cb9db897ddb4
Victor Quinones <victhorq716@gmail.com>
2026-04-17 13:49:42 -0400

    feat: modularize homepage components and update UI styling conventions & add new design hero component

--- src/modules/home/components/SocialLinks/SocialContainer.tsx
+++ src/modules/home/components/SocialLinks/SocialContainer.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import clsx from 'clsx';
+import React, { useState } from 'react'
+import clsx from 'clsx'
 
 interface SocialButtonProps {
-  description: string;
-  icon: React.ElementType;
-  onClick?: () => void;
-  url?: string;
-  hoverColor?: string;
-  ariaLabel?: string;
-  social?: string;
+  description: string
+  icon: React.ElementType
+  onClick?: () => void
+  url?: string
+  hoverColor?: string
+  ariaLabel?: string
+  social?: string
 }
 
 export const SocialContainer: React.FC<SocialButtonProps> = ({
@@ -20,15 +20,15 @@ export const SocialContainer: React.FC<SocialButtonProps> = ({
   ariaLabel = 'social-button',
   social = 'default',
 }) => {
-  const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState(false)
 
   const handleClick = () => {
     if (url) {
-      window.open(url, '_blank', 'noopener,noreferrer');
+      window.open(url, '_blank', 'noopener,noreferrer')
     } else if (onClick) {
-      onClick();
+      onClick()
     }
-  };
+  }
 
   const borderColor = clsx(
     social === 'discord'
@@ -36,15 +36,15 @@ export const SocialContainer: React.FC<SocialButtonProps> = ({
         ? 'border-[#ff6900]'
         : 'border-transparent'
       : social === 'redes'
-      ? isHovered
-        ? 'border-transparent'
-        : 'border-[#6b8cff]'
-      : isHovered
-      ? 'border-transparent'
-      : social === 'github'
-      ? 'border-gray-300'
-      : 'border-[#94e7f8]'
-  );
+        ? isHovered
+          ? 'border-transparent'
+          : 'border-[#00e5ff]'
+        : isHovered
+          ? 'border-transparent'
+          : social === 'github'
+            ? 'border-gray-300'
+            : 'border-[#94e7f8]',
+  )
 
   const textColor = clsx(
     social === 'discord'
@@ -52,26 +52,29 @@ export const SocialContainer: React.FC<SocialButtonProps> = ({
         ? 'text-[#ff6900]'
         : 'text-white'
       : social === 'redes'
-      ? isHovered
-        ? 'text-[#19243e]'
-        : 'text-[#6b8cff]'
-      : isHovered
-      ? 'text-white'
-      : ''
-  );
+        ? isHovered
+          ? 'text-[#19243e]'
+          : 'text-[#00e5ff]'
+        : isHovered
+          ? 'text-white'
+          : '',
+  )
 
-  const baseStyle = social === 'discord'
-    ? (isHovered ? { background: 'transparent' } : { background: '#ff6900' })
-    : isHovered
-    ? { background: hoverColor ?? 'rgba(255, 255, 255, 0.1)' }
-    : {};
+  const baseStyle =
+    social === 'discord'
+      ? isHovered
+        ? { background: 'transparent' }
+        : { background: '#ff6900' }
+      : isHovered
+        ? { background: hoverColor ?? 'rgba(255, 255, 255, 0.1)' }
+        : {}
 
   return (
     <div
       className={clsx(
         'flex items-center text-lg active:scale-95 h-12 justify-center cursor-pointer rounded-md w-full sm:w-auto font-semibold transition-all duration-200 border p-4',
         borderColor,
-        textColor
+        textColor,
       )}
       style={baseStyle}
       onMouseEnter={() => setIsHovered(true)}
@@ -80,12 +83,11 @@ export const SocialContainer: React.FC<SocialButtonProps> = ({
     >
       <button
         aria-label={ariaLabel}
-        className="flex items-center gap-2 w-full justify-center cursor-pointer lg:px-10 h-full border-none bg-transparent"
+        className='flex items-center gap-2 w-full justify-center cursor-pointer lg:px-10 h-full border-none bg-transparent'
       >
         {description}
-        <Icon className="lg:w-15 lg:h-7" />
+        <Icon className='lg:w-15 lg:h-7' />
       </button>
     </div>
-  );
-};
-
+  )
+}
--- src/modules/home/components/SocialLinks/SocialLinks.tsx
+++ src/modules/home/components/SocialLinks/SocialLinks.tsx
@@ -1,58 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import { createPortal } from 'react-dom';
-import { socialLinks } from 'src/modules/about-us/constants/about-us.const';
-import { SocialContainer } from './SocialContainer';
-import { EntryForm } from '../EntryForm/EntryForm';
+import React, { useEffect, useState } from 'react'
+import { createPortal } from 'react-dom'
+import { socialLinks } from 'src/modules/about-us/constants/about-us.const'
+import { SocialContainer } from './SocialContainer'
+import { EntryForm } from '../EntryForm/EntryForm'
 
 export const SocialLinks: React.FC = () => {
-	const [isFormOpen, setIsFormOpen] = useState(false);
+  const [isFormOpen, setIsFormOpen] = useState(false)
 
-	const openForm = () => {
-		setIsFormOpen((prev) => !prev);
-	};
+  const openForm = () => {
+    setIsFormOpen((prev) => !prev)
+  }
 
-	useEffect(() => {
-		if (isFormOpen) {
-			document.body.classList.add('overflow-hidden');
-		} else {
-			document.body.classList.remove('overflow-hidden');
-		}
-	}, [isFormOpen]);
+  useEffect(() => {
+    if (isFormOpen) {
+      document.body.classList.add('overflow-hidden')
+    } else {
+      document.body.classList.remove('overflow-hidden')
+    }
+  }, [isFormOpen])
 
-	const discord = socialLinks.discord;
-	const github = socialLinks.github;
+  const discord = socialLinks.discord
+  const github = socialLinks.github
 
-	return (
-		<div className='flex justify-center gap-4 mt-2 w-full'>
-			<div className='flex flex-row flex-wrap justify-center gap-4'>
-				<SocialContainer
-					description={discord.description}
-					icon={discord.icon}
-					onClick={openForm}
-					hoverColor={'#5865F2'}
-					ariaLabel={discord.title}
-					social='discord'
-				/>
-				<SocialContainer
-					description={github.description}
-					icon={github.icon}
-					url={github.url}
-					hoverColor={'#6b8cff'}
-					ariaLabel={github.title}
-					social='redes'
-				/>
-			</div>
+  return (
+    <div className='flex justify-center gap-4 mt-2 w-full'>
+      <div className='flex flex-row flex-wrap justify-center gap-4'>
+        <SocialContainer
+          description={discord.description}
+          icon={discord.icon}
+          onClick={openForm}
+          hoverColor={'#5865F2'}
+          ariaLabel={discord.title}
+          social='discord'
+        />
+        <SocialContainer
+          description={github.description}
+          icon={github.icon}
+          url={github.url}
+          hoverColor={'#00e5ff'}
+          ariaLabel={github.title}
+          social='redes'
+        />
+      </div>
 
-			{isFormOpen && typeof window !== 'undefined'
-				? createPortal(
-						<section className='left-0 top-0 backdrop-blur-3xl flex justify-center items-center rounded fixed h-[100dvh] w-[100vw] z-[999] shadow'>
-							<div className='bg-white-5 rounded-lg shadow-lg  '>
-								<EntryForm onClose={() => setIsFormOpen(false)} />
-							</div>
-						</section>,
-						document.body
-				  )
-				: null}
-		</div>
-	);
-};
+      {isFormOpen && typeof window !== 'undefined'
+        ? createPortal(
+            <section className='left-0 top-0 backdrop-blur-3xl flex justify-center items-center rounded fixed h-[100dvh] w-[100vw] z-[999] shadow'>
+              <div className='bg-white-5 rounded-lg shadow-lg  '>
+                <EntryForm onClose={() => setIsFormOpen(false)} />
+              </div>
+            </section>,
+            document.body,
+          )
+        : null}
+    </div>
+  )
+}
