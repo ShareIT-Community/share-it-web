fd7087f8eeb2b8153a18132ae825cbf50caf5dba
Victor Quinones <victhorq716@gmail.com>
2026-04-18 10:09:34 -0400

    feat: configure site URL, and enhance SEO metadata in Layout component

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://share-it.tech',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
