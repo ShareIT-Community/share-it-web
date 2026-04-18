732bf6965ad74a982b5fb0ba57c83b6fad3bcf1d
Victor Quinones <victhorq716@gmail.com>
2026-04-18 16:43:08 -0400

    feat: redesign rules page and community cards with updated branding

--- src/modules/rules/components/RulesLineItem.tsx
+++ src/modules/rules/components/RulesLineItem.tsx
@@ -12,18 +12,18 @@ interface Props {
 export const RulesLineItem = ({ data = [] }: Props) => {
 	return (
 		<ol
-			className='relative space-y-8 before:absolute before:top-0
-		before:left-[5px] sm:before:left-1/2 before:h-full before:w-0.5 sm:before:-translate-x-1/2 before:rounded-full before:bg-gray-700'>
+			className='relative space-y-12 before:absolute before:top-0
+		before:left-[5px] sm:before:left-1/2 before:h-full before:w-px sm:before:-translate-x-1/2 before:bg-gradient-to-b before:from-[#00e5ff]/50 before:via-[#00e5ff]/10 before:to-transparent'>
 			{data.map((group) => (
 				<li
-					className='group relative grid sm:grid-cols-2 sm:odd:-me-3 sm:even:-ms-3 ml-4 sm:ml-0'
+					className='group relative grid sm:grid-cols-2 sm:odd:-me-3 sm:even:-ms-3 ml-6 sm:ml-0'
 					key={group.title}>
-					<div className='relative flex items-start gap-4 sm:group-odd:flex-row-reverse sm:group-odd:text-right sm:group-even:order-last'>
-						<span className='size-3 shrink-0 rounded-full bg-blue-600 mt-1 md:mt-0'></span>
+					<div className='relative flex items-start gap-6 sm:group-odd:flex-row-reverse sm:group-odd:text-right sm:group-even:order-last'>
+						<span className='size-3 shrink-0 rounded-full bg-[#00e5ff] shadow-[0_0_10px_#00e5ff] mt-2 sm:mt-1'></span>
 
-						<div className='-mt-2'>
-							<h3 className='text-lg font-bold text-white'>{group.title}</h3>
-							<p className='mt-0.5 text-sm text-gray-200'>
+						<div className='flex-1'>
+							<h3 className='text-xl font-bold text-white group-hover:text-[#00e5ff] transition-colors duration-300'>{group.title}</h3>
+							<p className='mt-2 text-md text-gray-400 leading-relaxed'>
 								{group.description}
 							</p>
 						</div>
